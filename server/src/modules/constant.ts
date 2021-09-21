2616033a0a402f20802789da09a3159a9172b382
Alfin Aditya <alfinaditia02@gmail.com>
2021-09-21 09:34:28 +0700

    add staging prod

--- server/src/modules/constant.ts
+++ server/src/modules/constant.ts
@@ -1,4 +1,7 @@
 export const prod =
-  process.env.NODE_ENV === 'production'
+  (process.env.NODE_ENV === 'production'
     ? 'https://yukbisa.netlify.app/'
-    : 'http://localhost:3000/'
+    : 'http://localhost:3000/') ||
+  (process.env.NODE_ENV === 'staging'
+    ? 'https://staging-yukbisa.netlify.app/'
+    : 'http://localhost:3000/')
